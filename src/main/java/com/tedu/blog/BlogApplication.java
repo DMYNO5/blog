cf796b4906e08f7f3794b76afa5306cb58e262db
hothand <50542608+hothand@users.noreply.github.com>
2020-11-22 09:13:06 +0800

    扫描mapper文件

--- src/main/java/com/tedu/blog/BlogApplication.java
+++ src/main/java/com/tedu/blog/BlogApplication.java
@@ -1,8 +1,9 @@
 package com.tedu.blog;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("com.tedu.blog.mapper")
 @SpringBootApplication
 public class BlogApplication {
 
